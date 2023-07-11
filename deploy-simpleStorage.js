40f3569ac1cea0acbc495d1d538f8be61ee6fef6
Dhhruvvv <118907757+Bhavik-punmiya@users.noreply.github.com>
2023-07-11 10:23:58 +0530

    Simple-Storage
    This is SimpleStorage.js code. It works fine for ethers version less than 6 , follow
    ReadMe.md to deploy these contract

---
+++ deploy-simpleStorage.js
@@ -0,0 +1,37 @@
+const ethers = require("ethers")
+// const solc = require("solc")
+const fs = require("fs-extra")
+require("dotenv").config()
+
+async function main() {
+    //Deploying the Contract 
+    let provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+    let wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
+    const binary = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.bin",
+        "utf8"
+    )
+    const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+    console.log("Deploying, please wait...")
+    const contract = await contractFactory.deploy()
+    console.log(`Contract deployed to ${contract.address}`)
+    const deploymentReceipt = await contract.deployTransaction
+
+    //Retrieving the currentFavoriteNumber
+    let currentFavoriteNumber = await contract.retrieve()
+    console.log(`Current Favorite Number: ${currentFavoriteNumber}`)
+    //Updateing the favorite number
+    console.log("Updating favorite number...")
+    let transactionResponse = await contract.store(7)
+    let transactionReceipt = await transactionResponse.wait(1)
+    currentFavoriteNumber = await contract.retrieve()
+    console.log(`New Favorite Number: ${currentFavoriteNumber}`)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
